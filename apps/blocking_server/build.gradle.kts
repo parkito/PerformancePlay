dc8c0469e91f1bada50b4d7e7a7f08a3320de774
parkito <artyom-karnov@yandex.ru>
2020-03-08 01:32:02 -0700

    Add spring dependency

--- apps/blocking_server/build.gradle.kts
+++ apps/blocking_server/build.gradle.kts
@@ -1,25 +1,23 @@
-group = "ru.siksmfp.kotlin.areon"
+group = "ru.siksmfp.server.blocking"
 version = "0.1.1"
-
-val appArchiveName = "areon"
 
 plugins {
     application
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.70"
+    id("org.springframework.boot") version "2.2.5.RELEASE"
+    kotlin("plugin.spring") version "1.3.61"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-application {
-    mainClassName = "samples.HelloWorldKt"
-}
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 dependencies {
-    compile(kotlin("stdlib"))
-    compile("io.aeron:aeron-all:1.19.1")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.postgresql:postgresql:42.2.10")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testCompile("org.junit.jupiter:junit-jupiter-engine:5.4.1")
 }
 
@@ -28,10 +26,9 @@ repositories {
     mavenCentral()
 }
 
-val jar by tasks.getting(Jar::class) {
-    archiveName = "$appArchiveName.jar"
-    into("META-INF") {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
     }
 }
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }

3be275d64351b5c4435eb764b7037403aeb00be0
parkito <artyom-karnov@yandex.ru>
2020-03-16 23:32:52 -0600

    Add test files

--- apps/blocking_server/src/test/kotlin/ru/siksmfp/server/blocking/config/MessageControllerTest.kt
+++ apps/blocking_server/src/test/kotlin/ru/siksmfp/server/blocking/config/MessageControllerTest.kt
@@ -1,5 +1,16 @@
 package ru.siksmfp.server.blocking.config
 
-class ParentTest{
+import org.jsmart.zerocode.core.domain.JsonTestCase
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(ZeroCodeUnitRunner::class)
+class MessageControllerTest {
+
+    @Test
+    @JsonTestCase("load_tests/get/get_screening_details_by_custid.json")
+    @Throws(Exception::class)
+    fun testGetScreeningLocalAndGlobal() {
+    }
 }
